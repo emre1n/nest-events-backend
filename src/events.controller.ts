2f6c712b46db436a58fa150eb48d4bf290224091
emre1n <emre1n@protonmail.com>
2023-07-20 16:01:51 +0300

    add DTOs create and update

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,17 +1,38 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateEventDto } from './create-event-dto';
+import { UpdateEventDto } from './update-event.dto';
 
 @Controller('/events')
 export class EventsController {
   @Get()
-  findAll() {}
+  findAll() {
+    return [
+      { id: 1, name: 'First event' },
+      { id: 2, name: 'Second event' },
+    ];
+  }
   @Get(':id')
   findOne(@Param('id') id) {
-    return id;
+    return { id: 1, name: 'First event' };
   }
   @Post()
-  create() {}
+  create(@Body() input: CreateEventDto) {
+    return input;
+  }
   @Patch(':id')
-  update(@Param('id') id) {}
+  update(@Param('id') id, @Body() input: UpdateEventDto) {
+    return input;
+  }
   @Delete(':id')
+  @HttpCode(204)
   remove(@Param('id') id) {}
 }
