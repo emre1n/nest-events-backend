ac31aa6de05ba42a161c647cc54319fbf99d7472
emre1n <emre1n@protonmail.com>
2023-07-20 15:17:39 +0300

    add EventsController with CRUD actions and route parameters

---
+++ src/events.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+
+@Controller('/events')
+export class EventsController {
+  @Get()
+  findAll() {}
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return id;
+  }
+  @Post()
+  create() {}
+  @Patch(':id')
+  update(@Param('id') id) {}
+  @Delete(':id')
+  remove(@Param('id') id) {}
+}
