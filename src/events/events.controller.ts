c1139861826b36330c78d00417b6dcf7c1dd8d1c
emre1n <emre1n@protonmail.com>
2023-07-21 16:38:32 +0300

    add providers and dependency injection

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -8,7 +8,6 @@ import {
   ParseIntPipe,
   Patch,
   Post,
-  ValidationPipe,
 } from '@nestjs/common';
 import { CreateEventDto } from './create-event-dto';
 import { UpdateEventDto } from './update-event.dto';
