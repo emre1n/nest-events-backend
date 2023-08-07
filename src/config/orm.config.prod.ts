677f768d717e8e13a27022ade794ab24ae38dacd
emre1n <emre1n@protonmail.com>
2023-08-07 15:40:55 +0300

    add initial tests

--- src/config/orm.config.prod.ts
+++ src/config/orm.config.prod.ts
@@ -14,5 +14,6 @@ export default registerAs(
     database: process.env.DB_DATABASE,
     entities: [Event, Attendee],
     synchronize: false,
+    dropSchema: false,
   }),
 );
--- test/events.e2e-spec.ts
+++ test/events.e2e-spec.ts
@@ -2,9 +2,27 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import * as request from 'supertest';
+import { Connection } from 'typeorm';
+import * as fs from 'fs';
+import * as path from 'path';
+import { Length } from 'class-validator';
 
 let app: INestApplication;
 let mod: TestingModule;
+let connection: Connection;
+
+const loadFixtures = async (sqlFileName: string) => {
+  const sql = fs.readFileSync(
+    path.join(__dirname, 'fixtures', sqlFileName),
+    'utf8',
+  );
+
+  const queryRunner = connection.driver.createQueryRunner('master');
+
+  for (const c of sql.split(';')) {
+    await queryRunner.query(c);
+  }
+};
 
 describe('Events (e2e)', () => {
   beforeAll(async () => {
@@ -14,6 +32,8 @@ describe('Events (e2e)', () => {
 
     app = mod.createNestApplication();
     await app.init();
+
+    connection = app.get(Connection);
   });
 
   afterAll(async () => {
@@ -21,6 +41,24 @@ describe('Events (e2e)', () => {
   });
 
   it('should return an empty list of events', async () => {
-    return request(app.getHttpServer()).get('/events').expect(200);
+    return request(app.getHttpServer())
+      .get('/events')
+      .expect(200)
+      .then((response) => {
+        expect(response.body.data.length).toBe(0);
+      });
+  });
+
+  it('should return a single event', async () => {
+    await loadFixtures('1-event-1-user.sql');
+
+    return request(app.getHttpServer())
+      .get('/events/1')
+      .expect(200)
+      .then((response) => {
+        expect(response.body.id).toBe(1);
+        expect(response.body.name).toBe('Interesting Party');
+        console.log(response.body);
+      });
   });
 });
