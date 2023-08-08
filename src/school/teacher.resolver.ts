427c2a6ec3f0e76b04f3ef90856871ec36d4c376
emre1n <emre1n@protonmail.com>
2023-08-08 16:13:25 +0300

    add teacher resolver and test with playground

---
+++ src/school/teacher.resolver.ts
@@ -0,0 +1,17 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { Teacher } from './teacher.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Resolver(() => Teacher)
+export class TeacherResolver {
+  constructor(
+    @InjectRepository(Teacher)
+    private readonly teachersRepository: Repository<Teacher>,
+  ) {}
+
+  @Query(() => [Teacher])
+  public async teachers(): Promise<Teacher[]> {
+    return await this.teachersRepository.find();
+  }
+}
