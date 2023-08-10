d60cf59e37bba93eb5440beeeb4cd73f14ffecf7
emre1n <emre1n@protonmail.com>
2023-08-10 14:34:02 +0300

    add delete teacher mutation

--- src/school/teacher.resolver.ts
+++ src/school/teacher.resolver.ts
@@ -12,6 +12,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { TeacherAddInput } from './input/teacher-add.input';
 import { Logger } from '@nestjs/common';
+import { TeacherEditInput } from './input/teacher-edit.input';
+import { EntityWithId } from './school.types';
 
 @Resolver(() => Teacher)
 export class TeacherResolver {
@@ -38,12 +40,45 @@ export class TeacherResolver {
       },
     });
   }
+
   @Mutation(() => Teacher, { name: 'teacherAdd' })
   public async add(
     @Args('input', { type: () => TeacherAddInput })
     input: TeacherAddInput,
   ): Promise<Teacher> {
     return await this.teachersRepository.save(new Teacher(input));
+  }
+
+  @Mutation(() => Teacher, { name: 'teacherEdit' })
+  public async edit(
+    @Args('id', { type: () => Int })
+    id: number,
+    @Args('input', { type: () => TeacherEditInput })
+    input: TeacherEditInput,
+  ): Promise<Teacher> {
+    const teacher = await this.teachersRepository.findOneOrFail({
+      where: {
+        id,
+      },
+    });
+    return await this.teachersRepository.save(
+      new Teacher(Object.assign(teacher, input)),
+    );
+  }
+
+  @Mutation(() => EntityWithId, { name: 'teacherDelete' })
+  public async delete(
+    @Args('id', { type: () => Int })
+    id: number,
+  ): Promise<EntityWithId> {
+    const teacher = await this.teachersRepository.findOneOrFail({
+      where: {
+        id,
+      },
+    });
+    await this.teachersRepository.remove(teacher);
+
+    return new EntityWithId(id);
   }
 
   @ResolveField('subjects')
