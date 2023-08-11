747c7ec5ad057be48a33ae5526a8f85b22a43a35
emre1n <emre1n@protonmail.com>
2023-08-11 17:18:27 +0300

    add user already exist custom validator

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user.entity';
-import { LocalStrategy } from './local.strategy';
-import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthController } from './auth.controller';
+import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
-import { UsersController } from './users.controller';
-import { AuthResolver } from './auth.resolver';
+import { LocalStrategy } from './local.strategy';
+import { User } from './user.entity';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
+import { UsersController } from './users.controller';
+import { UserDoesNotExistConstraint } from './validation/user-does-not-exists.constraint';
 
 @Module({
   imports: [
@@ -30,6 +31,7 @@ import { UserService } from './user.service';
     AuthResolver,
     UserResolver,
     UserService,
+    UserDoesNotExistConstraint,
   ],
   controllers: [AuthController, UsersController],
 })
