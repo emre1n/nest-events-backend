8752f0552f5e50e8f8073462e945d5605ba6f6b7
emre1n <emre1n@protonmail.com>
2023-08-15 14:16:31 +0300

    add pagination gql

--- src/school/teacher.entity.ts
+++ src/school/teacher.entity.ts
@@ -9,6 +9,7 @@ import {
 import { Subject } from './subject.entity';
 import { Gender } from './school.types';
 import { Course } from './course.entity';
+import { Paginated } from 'src/pagination/paginator';
 
 @Entity()
 @ObjectType()
@@ -41,3 +42,6 @@ export class Teacher {
   @Field(() => [Course])
   courses: Promise<Course[]>;
 }
+
+@ObjectType()
+export class PaginatedTeachers extends Paginated<Teacher>(Teacher) {}
